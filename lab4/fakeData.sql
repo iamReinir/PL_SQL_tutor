ef2ef4a2dc27452966e90a98d146d8d8a4ebaee5
Reinir <blackwhitenxt@gmail.com>
2023-03-11 19:09:28 +0700

    bugs fix

--- lab4/fakeData.sql
+++ lab4/fakeData.sql
@@ -1,7 +1,5 @@
-INSERT INTO departments (code, dept_name) VALUES (
-    ('001','human resource'),
-    ('002','finance'),
-    ('003','reseach and development'),
-    ('004','marketing'),
-    ('005','production'),
-);
+INSERT INTO departments (code, dept_name) VALUES ('001','human resource');
+INSERT INTO departments (code, dept_name) VALUES ('002','finance');
+INSERT INTO departments (code, dept_name) VALUES ('003','reseach and development');
+INSERT INTO departments (code, dept_name) VALUES ('004','marketing');
+INSERT INTO departments (code, dept_name) VALUES ('005','production');
