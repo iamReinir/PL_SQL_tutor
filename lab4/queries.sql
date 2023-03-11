fb550c423b0ee22d7cd3ca612d35c1e4b60b5a3c
Reinir <blackwhitenxt@gmail.com>
2023-03-11 19:42:44 +0700

    queries

--- lab4/queries.sql
+++ lab4/queries.sql
@@ -1,6 +1,22 @@
 /*
 1. Ai là quản lý phòng ban có tên là "Phòng nghiên cứu và phát triên"
 */
-SELECT employees.code, employees.fullname, departments.code, departments.dept_name
+
+/*Query này lấy tên manager của department bất kỳ*/
+SELECT
+    employees.code as employee_code,
+    employees.fullname,
+    departments.code as department_code,
+     departments.dept_name as department_name
 FROM
-    (employees JOIN departments);
+    (employees JOIN departments ON employees.managerOf = departments.code)
+WHERE
+    dept_name = 'research and development';/* Chỗ này tên gì cũng được */
+
+SELECT
+    projects.code as project_code,
+    proj_name as project_name,
+    dept_name as department_name
+FROM
+    (departments JOIN projects ON department.code = managingDept)
+WHERE dept_name = 'research and development';
