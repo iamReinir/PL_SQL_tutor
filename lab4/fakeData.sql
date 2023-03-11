0ff97df4584738d5395e9e31e62a99f2d2a2d644
Reinir <blackwhitenxt@gmail.com>
2023-03-11 19:17:13 +0700

    fake data

--- lab4/fakeData.sql
+++ lab4/fakeData.sql
@@ -3,3 +3,18 @@ INSERT INTO departments (code, dept_name) VALUES ('002','finance');
 INSERT INTO departments (code, dept_name) VALUES ('003','reseach and development');
 INSERT INTO departments (code, dept_name) VALUES ('004','marketing');
 INSERT INTO departments (code, dept_name) VALUES ('005','production');
+
+INSERT INTO employees (code, fullname, gender, dateOfBirth, department, managerOf)
+    VALUES ('001','mr jame','male','08-06-1996','001',NULL);
+INSERT INTO employees (code, fullname, gender, dateOfBirth, department, managerOf)
+    VALUES ('002','jmmy','male','08-06-1996','002',NULL);
+INSERT INTO employees (code, fullname, gender, dateOfBirth, department, managerOf)
+    VALUES ('003','mr jame','male','08-06-1996','003',NULL);
+INSERT INTO employees (code, fullname, gender, dateOfBirth, department, managerOf)
+    VALUES ('004','mr jame','male','08-06-1996','004',NULL);
+INSERT INTO employees (code, fullname, gender, dateOfBirth, department, managerOf)
+    VALUES ('005','mr jame','male','08-06-1996','005',NULL);
+INSERT INTO employees (code, fullname, gender, dateOfBirth, department, managerOf)
+    VALUES ('006','mr jame','male','08-06-1996','006',NULL);
+INSERT INTO employees (code, fullname, gender, dateOfBirth, department, managerOf)
+    VALUES ('007','mr jame','male','08-06-1996','001',NULL);
