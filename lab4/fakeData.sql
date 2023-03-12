ee37aff329ea634889710ba5a7dd5d9925705ca8
Reinir <blackwhitenxt@gmail.com>
2023-03-12 13:45:05 +0700

    commit number idk

--- lab4/fakeData.sql
+++ lab4/fakeData.sql
@@ -1,8 +1,9 @@
-INSERT INTO departments (code, dept_name) VALUES ('001','human resource');
-INSERT INTO departments (code, dept_name) VALUES ('002','finance');
-INSERT INTO departments (code, dept_name) VALUES ('003','reseach and development');
-INSERT INTO departments (code, dept_name) VALUES ('004','marketing');
-INSERT INTO departments (code, dept_name) VALUES ('005','production');
+INSERT INTO departments (code, dept_name) VALUES
+    ('D001','human resource'),
+    ('D002','finance'),
+    ('D003','reseach and development'),
+    ('D004','marketing'),
+    ('D005','production');
 
 INSERT INTO employees (code, fullname, gender, dateOfBirth, department, managerOf)
     VALUES ('001','mr jame','male','08-jan-1996','001',NULL);
@@ -20,3 +21,22 @@ INSERT INTO employees (code, fullname, gender, dateOfBirth, department, managerO
     VALUES ('007','Aris','female','08-jan-1996','001',NULL);
 INSERT INTO employees (code, fullname, gender, dateOfBirth, department, managerOf)
     VALUES ('008','Alicia','female','08-jan-1996','003','003');
+
+supervise
+INSERT INTO supervise (supervisor, supervised, relation) VALUES ('E001', 'E002', 'Direct manager');
+INSERT INTO supervise (supervisor, supervised, relation) VALUES ('E001', 'E003', 'Indirect manager');
+INSERT INTO supervise (supervisor, supervised, relation) VALUES ('E002', 'E004', 'Mentor');
+INSERT INTO supervise (supervisor, supervised, relation) VALUES ('John', 'Jane', 'Team Leader');
+INSERT INTO supervise (supervisor, supervised, relation) VALUES ('Alice', 'Bob', 'Junior Analyst');
+INSERT INTO supervise (supervisor, supervised, relation) VALUES ('Mr B', 'Mr A', 'Senior Analyst');
+Project
+INSERT INTO projects(code, proj_name, proj_location, managingDept) VALUES ('P001', 'Solar Power Plant', 'Arizona', 'D001');
+INSERT INTO projects(code, proj_name, proj_location, managingDept) VALUES ('P002', 'Wind Farm', 'Texas', 'D002');
+INSERT INTO projects(code, proj_name, proj_location, managingDept) VALUES ('P003', 'Hydroelectric Dam', 'Washington', 'D003');
+INSERT INTO projects(code, proj_name, proj_location, managingDept) VALUES ('P004', 'Nuclear Power Plant', 'California', 'D004');
+INSERT INTO projects(code, proj_name, proj_location, managingDept) VALUES ('P005', 'Geothermal Power Plant', 'Hawaii', 'D005');
+INSERT INTO projects(code, proj_name, proj_location, managingDept) VALUES ('P006', 'Biomass Power Plant', 'Oregon', 'D006');
+Project join
+INSERT INTO projectJoin(employee, project, total_hour, stillInProject) VALUES ('001', 'P001', 40, 1);
+INSERT INTO projectJoin(employee, project, total_hour, stillInProject) VALUES ('002', 'P002', 30, 1);
+INSERT INTO projectJoin(employee, project, total_hour, stillInProject) VALUES ('003', 'P003', 20, 0);
--- lab4/queries.sql
+++ lab4/queries.sql
@@ -1,5 +1,6 @@
 /*
-1. Ai là quản lý phòng ban có tên là "Phòng nghiên cứu và phát triên"
+Q1. Ai là quản lý phòng ban có tên là "Phòng nghiên cứu và phát triên"
+Thông tin yêu cầu: mã số, họ tên nhân viên, mã số phòng ban, tên phòng ban
 "Nghiên cứu và phát triển" --> "research and development"
 */
 SELECT
@@ -13,7 +14,8 @@ WHERE
     dept_name = 'research and development';
 
 /*
-2.Phòng Nghiên cứu và phát triển hiện đang quản lý dự án nào.
+Q2.Phòng Nghiên cứu và phát triển hiện đang quản lý dự án nào.
+Thông tin yêu cầu: mã số dụ án, tên dự án, tên phòng ban quản lý
 */
 
 SELECT
@@ -25,7 +27,8 @@ FROM
 WHERE dept_name = 'research and development';
 
 /*
-3.Dự án có tên ProjectB hiện đang được quản lý bởi phòng ban nào.
+Q3.Dự án có tên ProjectB hiện đang được quản lý bởi phòng ban nào.
+Thông tin yêu cầu: mã số dụ án, tên dự án, tên phòng ban quản lý
 */
 SELECT
     projects.code as project_code,
@@ -36,7 +39,8 @@ FROM
 WHERE proj_name = 'ProjectB';
 
 /*
-4.Những nhân viên nào đang bị giám sát bởi nhân viên có tên Mai Duy An.
+Q4.Những nhân viên nào đang bị giám sát bởi nhân viên có tên Mai Duy An.
+Thông tin yêu cầu: mã số nhân viên, họ tên nhân viên
 */
 
 SELECT
@@ -45,4 +49,163 @@ FROM
     (((SELECT code FROM employees WHERE fullname='Mai Duy An') e_supervisor
     JOIN supervise
     ON supervise.supervisor = e_supervisor.code)
-    JOIN employees ON supervise.supervised = employees.code)
+    JOIN employees ON supervise.supervised = employees.code);
+
+/*
+Q5. ai hiện đang giám sát những nhân viên có tên Mai Duy An.
+Thông tin yêu cầu: mã số nhân viên, họ tên nhân viên giám sát.
+*/
+SELECT
+    employees.code, employees.fullname
+FROM
+    (((SELECT code FROM employees WHERE fullname='Mai Duy An') e_supervised
+    JOIN supervise
+    ON supervise.supervised = e_supervised.code)
+    JOIN employees ON supervise.supervisor = employees.code);
+
+/*
+Q6. dự án có tên ProjectA hiện đang làm việc ở đâu.
+Thông tin yêu cầu: mã số, tên vị trí làm việc.
+*/
+SELECT code, proj_location
+FROM projects WHERE proj_name='ProjectA';
+
+/*
+Q7. vị trí làm việc có tên Tp. HCM hiện đang là chỗ làm việc của những dự án nào.
+Thông tin yêu cầu: mã số, tên dự án
+*/
+
+SELECT code, proj_name
+FROM projects WHERE proj_location='tp.HCM';
+
+
+/*
+Q8. Cho biết những người phụ thuộc trên 18 tuổi .
+Thông tin yêu cầu: tên, ngày tháng năm sinh của người phụ thuộc, tên nhân viên phụ thuộc vào.
+*/
+
+SELECT
+    employee18.fullname, employee18.dateOfBirth, employees.fullname as supervisor_name
+FROM
+        ((SELECT
+            TRUNC((SYSDATE - TO_DATE(dateOfBirth, 'DD-Mon-YY'))/ 365.25) AS age,
+            dateOfBirth,
+            code,
+            fullname
+        FROM employees
+        WHERE age > 18) employee18 JOIN supervise ON employee18.code = supervise.supervised)
+    JOIN employees ON employees.code = supervise.supervisor;
+
+/*
+Q9.	Cho biết những người phụ thuộc là nam giới.
+Thông tin yêu cầu: tên, ngày tháng năm sinh của người phụ thuộc, tên nhân viên phụ thuộc vào
+*/
+
+SELECT male_emp.fullname, male_emp.dateOfBirth, employee.fullname as supervisor_name
+FROM
+        ((SELECT
+            dateOfBirth,
+            code,
+            fullname,
+        FROM employees
+        WHERE gender='male') male_emp JOIN supervise ON male_emp.code = supervise.supervised)
+    JOIN employees ON employees.code = supervise.supervisor;
+/*
+Q10.	Cho biết những nơi làm việc của phòng ban có tên : Phòng Nghiên cứu và phát triển.
+Thông tin yêu cầu: mã phòng ban, tên phòng ban, tên nơi làm việc.
+*/
+
+SELECT
+    departments.code as department_code,
+    dept_name,
+    proj_location as working_location
+FROM
+    projects JOIN departments ON managingDept = departments.code
+WHERE dept_name='research and development';
+
+/*
+Q11.	Cho biết các dự án làm việc tại Tp. HCM.
+Thông tin yêu cầu: mã dự án, tên dự án, tên phòng ban chịu trách nhiệm dự án.
+*/
+SELECT
+    projects.code as project_code,
+    proj_name,
+    dept_name as managing_department
+FROM
+    projects JOIN departments ON managingDept = departments.code
+WHERE proj_location='tp.HCM';
+
+/*
+Q12.	Cho biết những người phụ thuộc là nữ giới,
+của nhân viên thuộc phòng ban có tên: Phòng Nghiên cứu và phát triển .
+Thông tin yêu cầu: tên nhân viên, tên người phụ thuộc,
+mối liên hệ giữa người phụ thuộc với nhân viên.
+*/
+
+SELECT female_emp.fullname, employee.fullname as supervisor_name, relation
+FROM
+        ((SELECT employees.code, employees.name
+        FROM (employees JOIN departments ON employees.department=departments.code)
+        WHERE departments.dept_name='research and development'
+            AND gender='female') female_emp
+    JOIN supervise ON female_emp.code = supervise.supervised)
+    JOIN employees ON employees.code = supervise.supervisor;
+
+/*
+Q13.	Cho biết những người phụ thuộc trên 18 tuổi,
+của nhân viên thuộc phòng ban có tên: Phòng Nghiên cứu và phát triển.
+Thông tin yêu cầu: tên nhân viên, tên người phụ thuộc,
+mối liên hệ giữa người phụ thuộc với nhân viên.
+*/
+SELECT
+    employee18.fullname, employees.fullname as supervisor_name, relation
+FROM
+        ((SELECT
+            TRUNC((SYSDATE - TO_DATE(dateOfBirth, 'DD-Mon-YY'))/ 365.25) AS age,
+            dateOfBirth,
+            employees.code,
+            fullname
+        FROM employees JOIN departments ON employees.department=departments.code
+        WHERE age > 18 AND dept_name='research and development') employee18
+    JOIN supervise ON employee18.code = supervise.supervised)
+    JOIN employees ON employees.code = supervise.supervisor;
+
+/*
+Q14. Cho biết số lượng người phụ thuộc theo giới tính.
+Thông tin yêu cầu: giới tính, số lượng người phụ thuộc
+*/
+
+SELECT e_supervised.gender, COUNT(e_supervised.gender) AS total
+FROM
+    ((supervise
+    JOIN employee AS e_supervisor
+        ON supervise.supervisor=e_supervisor.code)
+    JOIN employee AS e_supervised
+        ON supervise.supervised=e_supervised.code)
+GROUP BY (e_supervised.gender);
+
+/*
+Q15. Cho biết số lượng người phụ thuộc theo mối liên hệ với nhân viên.
+Thông tin yêu cầu: mối liên hệ, số lượng người phụ thuộc
+*/
+SELECT relation, COUNT(relation) AS total
+FROM supervise
+GROUP BY relation;
+
+/*
+Q16.Cho biết số lượng người phụ thuộc theo mỗi phòng ban.
+Thông tin yêu cầu: mã phòng ban, tên phòng ban, số lượng người phụ thuộc
+*/
+
+SELECT e_supervised.department, departments.dept_name, COUNT(e_supervised.department) AS total
+FROM
+    (((supervise
+    JOIN employee AS e_supervisor
+        ON supervise.supervisor=e_supervisor.code)
+    JOIN employee AS e_supervised
+        ON supervise.supervised=e_supervised.code)
+    JOIN departments
+        ON e_supervised.department=departments.code)
+GROUP BY (e_supervised.department);X
+
+
