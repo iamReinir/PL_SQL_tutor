5a9902038194d312e5fb21448918d3ff9fd5f7bf
Reinir <blackwhitenxt@gmail.com>
2023-03-17 14:19:56 +0700

    .

--- lab5/procs.sql
+++ lab5/procs.sql
@@ -2,29 +2,58 @@
  1.Write function name: StudenID_ Func1 with parameter @mavt,
  return the sum of sl * giaban corresponding.
  */
-CREATE OR REPLACE FUNCTION QE170172_Func1(tar_vt in varchar2) RETURN int 
-IS 
+CREATE
+OR REPLACE FUNCTION QE170172_Func1(tar_vt in varchar2) RETURN int
+IS
 	total int := 0;
 	num int := 0;
 	price int := 0;
 BEGIN
-FOR vt IN( SELECT
-    sl, giaban   
-FROM
-    CHITIETHOADON
-WHERE
-    MaVT = tar_vt)
-LOOP
-total :=  total + vt.sl * vt.giaban;
-END LOOP;
+	FOR vt IN(
+		SELECT
+			sl,
+			giaban
+		FROM
+			CHITIETHOADON
+		WHERE
+			MaVT = tar_vt)
+	LOOP
+		total := total + vt.sl * vt.giaban;
+	END LOOP;
 return total;
 END;
 /
-
-drop function test;
-
-CREATE FUNCTION test RETURN int
+/*
+ 2.Write function to return a total of the HoaDon
+ (@MahD is a parameter)
+ */
+CREATE OR REPLACE
+FUNCTION total_HoaDon(code in varchar2) RETURN int
 IS
-	return 10;
+	result int := 0;
+BEGIN
+	FOR rec IN (SELECT sl,giaban FROM CHITIETHOADON WHERE MaHD=code)
+	loop
+		result := result + rec.sl*rec.giaban;
+	end loop;
+	return total;
 END;
 /
+
+/*
+3.	Write procedure name: StudenId _Proc1, parameter @makh, @diachi. This procedure
+help user update @diachi corresponding @makh.
+*/
+
+CREATE OR REPLACE
+procedure qe170172_proc1(
+  cust_code in varchar2,
+  address in varchar2
+)
+begin
+  update KHACHHANG
+	 set DiaChi=address
+   where MaKH=cust_code;
+end;
+/
+
